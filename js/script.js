ecab66df964e5126404039de47a0ee4dfc106e47
duckhang1412002 <nguyenduckhanglop6@gmail.com>
2022-10-01 23:26:57 +0700

    la bouffe

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function showMenu(x) {
+    if (document.getElementById("menu").style.display == "flex")
+      document.getElementById("menu").style.display = "none";
+    else document.getElementById("menu").style.display = "flex";
+}
